9edac833f815912efe27e425ed1c20187590cd6c
Sanad Liaquat <sanadhussain@gmail.com>
2015-08-04 15:39:01 +0500

    Removed unnecessary drop table command

--- db/migrate/20150707111429_create_spree_delivery_cities.rb
+++ db/migrate/20150707111429_create_spree_delivery_cities.rb
@@ -1,7 +1,5 @@
 class CreateSpreeDeliveryCities < ActiveRecord::Migration
   def change
-    drop_table :delivery_cities
-    drop_table :delivery_areas
 
     create_table :spree_delivery_cities do |t|
       t.string :name, null: false
