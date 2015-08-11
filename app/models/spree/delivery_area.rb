3e87439bddd0ebc73f2d3dd712fb3644533a748c
Sanad Liaquat <sanadhussain@gmail.com>
2015-08-11 12:15:55 +0500

    Added default sort order in scope

--- app/models/spree/delivery_area.rb
+++ app/models/spree/delivery_area.rb
@@ -1,4 +1,5 @@
 class Spree::DeliveryArea < ActiveRecord::Base
+  default_scope{ order('name ASC')}
   validates :name, presence: true
   belongs_to :delivery_city, class_name: "Spree::DeliveryCity"
 
